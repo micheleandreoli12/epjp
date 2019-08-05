caf81f72db5c31899d6ed9719d7187b7619f110e
Administrator <Administrator@192.168.12.59>
2019-08-05 16:27:34 +0200

    SQL beginning

--- scripts/s19.sql
+++ scripts/s19.sql
@@ -9,7 +9,7 @@ where region_id not in (2, 3);
 -- !! NOT IN(..., NULL) -> FALSE !!
 select *
 from regions
-where region_id not in (2, 3, null);
+where region_id not in (2, 3, null);--intervalli chiusi ai due lati es. 2 e 3 compresi
 
 select *
 from regions
@@ -18,3 +18,7 @@ where region_id between 2 and 3;
 select *
 from employees
 where manager_id is null;
+
+SELECT FIRST_NAME
+FROM EMPLOYEES
+WHERE SALARY BETWEEN 10000 AND 20000
